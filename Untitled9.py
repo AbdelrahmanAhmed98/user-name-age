a3a96761d08f957e5f6478006920220a278cf064
AbdelrahmanAhmed98 <81541624+AbdelrahmanAhmed98@users.noreply.github.com>
2021-04-05 22:56:27 +0200

    Add files via upload

---
+++ Untitled9.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+name = input('Enter your name: ')
+name
+age = int( input('Enter your age:'))
+age
+print('Hello', name+',','\nyour age is',age,'years old')
+
